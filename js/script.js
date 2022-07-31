d12a00c74906a29117b7788bd134d1ab218584de
mahmud035 <mahamudulhasan4148@gmail.com>
2022-07-31 11:32:50 +0600

    added email validation and show error message

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+function validateEmail() {
+  const email = document.getElementById('email').value;
+  const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const error = document.getElementById('error');
+
+  if (email.match(emailFormat)) {
+    return true;
+  } else {
+    error.innerHTML = 'Please check your email';
+    error.classList.add('error-message');
+    return false;
+  }
+}
+
+function validateSignUpEmail() {
+  const signUpEmail = document.getElementById('sign-up-email').value;
+  const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const errorText = document.getElementById('error-text');
+
+  if (signUpEmail.match(emailFormat)) {
+    return true;
+  } else {
+    errorText.innerHTML = 'Please check your email';
+    errorText.classList.add('error-message');
+    return false;
+  }
+}
